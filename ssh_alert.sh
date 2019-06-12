fb583088ca1d8b57b7dd8d8ffc29ed842d67e043
technicalkiwi <50688197+technicalkiwi@users.noreply.github.com>
2019-06-12 12:28:37 +1200

    Create ssh_alert.sh

---
+++ ssh_alert.sh
@@ -0,0 +1,16 @@
+ 
+TOKEN=######
+CHATID=########
+URL="https://api.telegram.org/bot$TOKEN/sendMessage"
+Text="
+A SSH login was successful, so here are some information for security:
+  User:        $PAM_USER
+	Date:        `date`
+	Server:      `uname -a`
+"
+if [ ${PAM_TYPE} = "open_session" ]; then
+	
+    curl -s --max-time $TIME -d "chat_id=$CHATID&disable_web_page_preview=1&text=$TEXT" $URL >/dev/null
+
+    fi
+exit 0
