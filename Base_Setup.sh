81a47b21a9146486d390ca101b63e243c499f859
technicalkiwi <50688197+technicalkiwi@users.noreply.github.com>
2019-06-14 13:56:13 +1200

    Update Base_Setup.sh

--- Base_Setup.sh
+++ Base_Setup.sh
@@ -1,17 +1,62 @@
+Skip to content
+ 
+Search or jump to…
+
+Pull requests
+Issues
+Marketplace
+Explore
+ 
+@technicalkiwi 
+0
+0 0 temightyspoon/Bash-Scripts
+ Code  Issues 0  Pull requests 0  Projects 0  Wiki  Security  Insights
+Bash-Scripts/Base_Setup.sh
+@temightyspoon temightyspoon update sed command
+d2d760b 29 minutes ago
+@temightyspoon @aarpk
+Executable File  65 lines (51 sloc)  1.99 KB
+    
 #!/bin/bash
 #script to install all the little thing on a new system
 
-read -p "please enter username for git" $usename
+#Setup Log Command
+log () {
+  if [ -n "$verbose" ]; then
+    eval "$@" |& tee -a /var/log/base_setup.log
+  else
+    eval "$@" |& tee -a /var/log/base_setup.log >/dev/null 2>&1
+  fi
+}
 
-read -p "please enter email for git" $email_addr
+#Setup Install Packages Command
+install_packages () {
+        for p in $PACKAGES; do
+        if dpkg -s "$p" >/dev/null 2>&1; then
+          echo "  * $p already installed"
+        else
+echo "  * Installing $p"
+log "apt-get install -y $p"
+ fi
+done;
+       }
 
-sudo add-apt-repository -y ppa:webupd8team/atom
+
+
+
+# Set Variables for script
+read -p "please enter username for git" $gitusername
+read -p "please enter email for git" $gitemail_addr
+read -p " What port do you want to use for ssh?" $SshPort
 
 sudo apt update -y
 sudo apt upgrade -y
-sudo apt autoremove
+sudo apt autoremove -y
 
-sudo apt install neofetch ssh htop chromium-browser vlc git net-tools curl wget unzip filezilla atom -y
+# Install Packages
+PACKAGES="neofetch ssh htop chromium-browser vlc git net-tools curl wget unzip filezilla"
+install_packages
+
 
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg;
   sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg;
@@ -19,7 +64,32 @@ curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microso
 	sudo apt-get update;
 	sudo apt-get install code;
 
-git config --global user.name $username
-git config --global user.email $email_addr
+# Configure git
+git config --global user.name $gitusername
+git config --global user.email $gitemail_addr
 git config --sysyem core.editor nano
 
+# Pull down Auth Keys
+curl https://raw.githubusercontent.com/temightyspoon/liunx-bits/master/auth_keys > /tmp/auth_tmp
+
+# Configure sshd_config
+SSHD_Path="/etc/ssh/sshd_config"
+  sed -i '1 i\#Created By Base_Setup Script' "$SSHD_Path"
+  sed -i "s|^\\(PermitRootLogon=\\).*|\\1no|" "$SSHD_Path"
+  sed -i "s|^\\(Port=\\).*|\\1$SshPort|" "$SSHD_Path"
+  sed -i "s|^\\(DB_DATABASE=\\).*|\\1$DB_NAME|" "$SSHD_Path"
+  sed -i "s|^\\(DB_USERNAME=\\).*|\\1$DB_USER|" "$SSHD_Path"
+  sed -i "s|^\\(DB_PASSWORD=\\).*|\\1$DB_PASS|" "$SSHD_Path"
+  sed -i "s|^\\(APP_URL=\\).*|\\1http://$WEB_ADDR|" "$SSHD_Path"
+© 2019 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Help
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
