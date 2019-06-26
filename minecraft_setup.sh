82678977e01da069d8cd454337cb28c65448c5a6
temightyspoon <44456520+temightyspoon@users.noreply.github.com>
2019-06-26 13:32:13 +1200

    update comments

--- minecraft_setup.sh
+++ minecraft_setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #Script to install Minecraft server
 
+#create logging function
 log () {
   if [ -n "$verbose" ]; then
     eval "$@" |& tee -a /var/log/minecraft_setup.log
@@ -9,6 +10,7 @@ log () {
   fi
 }
 
+#function to create start.sh
 create_start.sh(){
  {echo "#!/bin/bash"
  echo ""
@@ -16,21 +18,22 @@ create_start.sh(){
 } >> start.sh
 }
 
-
+#update and upgrade apt
 sudo apt update && sudo apt upgrade -y
+sudo apt install wget unzip openjdk-8-jre-headless screen
 
-sudo apt install openjdk-8-jre-headless screen
-
+#create user to run server
 sudo useradd -G sudo  minecraft 
 sudo mkdir /home/minecraft/server
 
+#download server jar file
 cd /home/minecraft/server 
 wget https://launcher.mojang.com/v1/objects/d0d0fe2b1dc6ab4c65554cb734270872b72dadd6/server.jar
 
-
+#create startup script
 create_start.sh
-
 sudo chmod +x start.sh
 log ./start.sh
 
+#alter eula to be true
 sed -i "s|^\\(eula=\\).*|\\1"true"|" "eula.txt"
