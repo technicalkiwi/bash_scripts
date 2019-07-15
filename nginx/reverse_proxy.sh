b7a0000cd212096d6851fb025cfdddad5c691896
temightyspoon <44456520+temightyspoon@users.noreply.github.com>
2019-07-15 12:36:06 +1200

    update reverse proxy script

--- nginx/reverse_proxy.sh
+++ nginx/reverse_proxy.sh
@@ -22,6 +22,43 @@ echo }
   } >> "$URI.conf"
 }
 
+create_sslserverblock() {
+    {
+        echo server { 
+        echo        listen 443 ssl;
+        echo        listen [::]:443;
+
+        echo        server_name $URI www.$URI
+        echo        
+        echo         #SSL
+        echo        ssl_certificate /etc/letsencrypt/live/$URI/fullchain.pem;
+        echo        ssl_certificate_key /etc/letsencrypt/live/$URI/privkey.pem;
+        echo        ssl_trusted_certificate /etc/letsencrypt/live/$URI/chain.pem;
+        echo
+        echo
+        echo        # Logging
+        echo        access_log /var/log/nginx/$URI.access.log;
+
+        echo        #Reverse Proxy
+        echo        location / {
+        echo        proxy_pass http://$IP:$PORT;
+        echo        include conf_files/proxy.conf;
+        echo }
+echo }
+        echo
+        echo        #HTTP Redirect
+        echo    server {
+        echo            listen 80;
+        echo            listen [::]:80;
+
+        echo            server_name $URI www.$URI;
+        echo
+        echo            location / {
+        echo            return 301 https://$URI$request_uri;
+        echo }
+echo }
+    } >>$URI.conf
+}
 
 
 # Sets the variables
@@ -34,3 +71,5 @@ cd /etc/nginx/sites-enabled/
 create_serverblock
 
 certbot certonly --nginx -d $URI -d www.$URI
+
+create_sslserverblock
