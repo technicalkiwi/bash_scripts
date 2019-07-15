aa1f3c2c273192eb682a6599831b7b7e77327ef5
TechnicalKiwi <aaron@technicalkiwi.com>
2019-07-15 12:52:44 +1200

    fix missing ;

--- nginx/reverse_proxy.sh
+++ nginx/reverse_proxy.sh
@@ -3,19 +3,19 @@
 create_serverblock () {
   {
 echo server {
-echo		listen 80;
-echo		listen [::]:80;
+echo		listen 80\;
+echo		listen [::]:80\;
 
-echo		server_name $URI www.$URI;
+echo		server_name $URI www.$URI\;
 echo
 echo		# Proxy
 echo		location /app {
-echo		proxy_pass http://$IP:$PORT;
-echo		include conf_files/proxy.conf;
+echo		proxy_pass http://$IP:$PORT\;
+echo		include conf_files/proxy.conf\;
 echo	}
 
   echo		# logging
-  echo		access_log /var/log/nginx/$URI.access.log;
+  echo		access_log /var/log/nginx/$URI.access.log\;
    
 echo }
 
@@ -25,36 +25,36 @@ echo }
 create_sslserverblock() {
     {
         echo server { 
-        echo        listen 443 ssl;
-        echo        listen [::]:443;
+        echo        listen 443 ssl\;
+        echo        listen [::]:443\;
 
         echo        server_name $URI www.$URI
         echo        
         echo         #SSL
-        echo        ssl_certificate /etc/letsencrypt/live/$URI/fullchain.pem;
-        echo        ssl_certificate_key /etc/letsencrypt/live/$URI/privkey.pem;
-        echo        ssl_trusted_certificate /etc/letsencrypt/live/$URI/chain.pem;
+        echo        ssl_certificate /etc/letsencrypt/live/$URI/fullchain.pem\;
+        echo        ssl_certificate_key /etc/letsencrypt/live/$URI/privkey.pem\;
+        echo        ssl_trusted_certificate /etc/letsencrypt/live/$URI/chain.pem\;
         echo
         echo
         echo        # Logging
-        echo        access_log /var/log/nginx/$URI.access.log;
+        echo        access_log /var/log/nginx/$URI.access.log\;
 
         echo        #Reverse Proxy
         echo        location / {
-        echo        proxy_pass http://$IP:$PORT;
-        echo        include conf_files/proxy.conf;
+        echo        proxy_pass http://$IP:$PORT\;
+        echo        include conf_files/proxy.conf\;
         echo }
 echo }
         echo
         echo        #HTTP Redirect
         echo    server {
-        echo            listen 80;
-        echo            listen [::]:80;
+        echo            listen 80\;
+        echo            listen [::]:80\;
 
-        echo            server_name $URI www.$URI;
+        echo            server_name $URI www.$URI\;
         echo
         echo            location / {
-        echo            return 301 https://$URI$request_uri;
+        echo            return 301 https://$URI$request_uri\;
         echo }
 echo }
     } >>$URI.conf
@@ -73,3 +73,4 @@ create_serverblock
 certbot certonly --nginx -d $URI -d www.$URI
 
 create_sslserverblock
+
