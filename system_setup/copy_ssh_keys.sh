a13b0c0f36417a13405d743226e7b788e8bdea57
temightyspoon <50688197+technicalkiwi@users.noreply.github.com>
2019-09-19 15:25:15 +1200

    add in Kvantum section

---
+++ system_setup/copy_ssh_keys.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# This script will copy public keys from git hub and add them to the allowed_keys file
+
+curl https://raw.githubusercontent.com/technicalkiwi/Liunx_bits/master/auth_keys > /tmp/auth_tmp
+mkdir -p ~/.ssh && cp /tmp/auth_tmp ~/.ssh/authorized_keys
