b4ee3700a4b56b95cc31065070ab35d42856b27b
temightyspoon <44456520+temightyspoon@users.noreply.github.com>
2019-11-19 11:23:17 +1300

    update key URL

--- system_setup/copy_ssh_keys.sh
+++ system_setup/copy_ssh_keys.sh
@@ -2,5 +2,5 @@
 
 # This script will copy public keys from git hub and add them to the allowed_keys file
 
-curl https://raw.githubusercontent.com/technicalkiwi/Liunx_bits/master/auth_keys > /tmp/auth_tmp
+curl https://raw.githubusercontent.com/technicalkiwi/Linux_bits/master/auth_keys > /tmp/auth_tmp
 mkdir -p ~/.ssh && cp /tmp/auth_tmp ~/.ssh/authorized_keys
