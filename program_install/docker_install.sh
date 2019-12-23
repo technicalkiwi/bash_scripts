ee72d24eb1c7fb4bbfb34a65141ebe27f16c8550
technicalkiwi <50688197+technicalkiwi@users.noreply.github.com>
2019-12-24 11:25:33 +1300

    Add in install package command.

--- program_install/docker_install.sh
+++ program_install/docker_install.sh
@@ -1,22 +1,43 @@
 #!/bin/bash
+
+
+#Setup Install Packages Command
+install_packages () {
+        for p in $PACKAGES; do
+        if dpkg -s "$p" >/dev/null 2>&1; then
+          echo "  * $p already installed"
+        else
+echo "  * Installing $p"
+log "apt-get install -y $p"
+ fi
+done;
+       }
+
+
+# Update Apt cache
 sudo apt update
 
-#installs needed commands
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
+# Installs needed packages
+PACKAGES="apt-transport-https ca-certificates curl software-properties-common gnupg-agent" 
+install_packages
 
-#pulls key and adds docker repo
+# Pulls key and adds docker repo
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
-#updates apt repo and installs docker CE
+# Updates apt repo and installs docker CE
 sudo apt update
 sudo apt install docker-ce -y
 sudo usermod -aG docker $USER
 clear
 
-#displays docker version 
+# Displays docker version 
 docker -v
 
-#waits for key press before running hello-world
+# wWits for key press before running hello-world
 read -n 1 -s -r -p "Press any key to run your first docker"
 sudo docker container run hello-world
+
+# Install Docker Compose
+
+sudo apt install docker-compose -y
